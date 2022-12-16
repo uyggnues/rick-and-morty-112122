90b7a889a2dfd905ed2c0054f26ea0d147650cbc
Matteo Piccini <gnappo.1@live.it>
2022-12-16 11:38:43 -0800

    Turn characters.js into json file, and fetch data

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,24 @@ import './App.css';
 import characters from "../data/characters"
 import SearchForm from './SearchForm';
 import CharactersList from './CharactersList';
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 function App() {
   const [queryChar, setQueryChar] = useState("")
+  const [characters, setCharacters] = useState([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:3000/characters")
+      const characters = await response.json()
+      setCharacters(characters)
+    }
+    
+    fetchData()
+  }, [])
+  
   const filteredCharacters = characters.filter(character => character.name.toLowerCase().includes(queryChar))
-
-  // const conditionalRender = queryChar === "" ? characters : filteredCharacters
-
+  
   return (
     <div className="App">
       <SearchForm query={queryChar} setQueryChar={setQueryChar}/>
