71e5d525dfb2d6b7bcfadcf1f5e71dce3f534809
Matteo Piccini <gnappo.1@live.it>
2022-12-19 16:00:45 -0800

    add new char form and subsequent POST request

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,42 @@
 import logo from '../logo.svg';
+import React, {useState, useEffect} from 'react'
 import './App.css';
 import characters from "../data/characters"
 import SearchForm from './SearchForm';
 import CharactersList from './CharactersList';
-import React, {useState, useEffect} from 'react'
+import CharacterForm from './CharacterForm';
 
 function App() {
   const [queryChar, setQueryChar] = useState("")
   const [characters, setCharacters] = useState([]);
 
-  useEffect(() => {
+  console.log("a") // sync, runs first
+  useEffect(() => { // runs AFTER a component mounted
+    console.log("b") // first line inside, has to happen first since it's sync
     const fetchData = async () => {
-      const response = await fetch("http://localhost:3000/characters")
-      const characters = await response.json()
-      setCharacters(characters)
+      try {
+        const response = await fetch("http://localhost:3000/characters")
+        console.log("c") // last to run because wrapped inside async code
+        const characters = await response.json()
+        setCharacters(characters)
+      } catch (error) {
+        alert(error)
+      }
     }
     
     fetchData()
+    console.log("d") // second sync line inside, has to happen second
+    
   }, [])
-  
+  console.log("e") //sync runs second on initial mounting
+  // a, e, b, d, c final order of console logs
   const filteredCharacters = characters.filter(character => character.name.toLowerCase().includes(queryChar))
 
   return (
     <>
       <div className="App transparent">
         <SearchForm query={queryChar} setQueryChar={setQueryChar}/>
+        <CharacterForm setCharacters={setCharacters} />
         <CharactersList characters={filteredCharacters} />
       </div>
       <div className='cursor'></div>
---
+++ src/components/CharacterForm.js
@@ -0,0 +1,87 @@
+import { useState } from 'react'
+
+const CharacterForm = ({setCharacters}) => {
+    // const [name, setName] = useState("")
+    // const [image, setImage] = useState("")
+    // const [status, setStatus] = useState("")
+    // const [species, setSpecies] = useState("")
+    // const [type, setType] = useState("")
+    // const [gender, setGender] = useState("")
+
+    const [newCharacter, setNewCharacter] = useState({
+        name: "",
+        status: "",
+        species: "",
+        type: "",
+        gender: "",
+        image: ""
+    })
+
+    const handleChange = ({target: {name, value}}) => { //"Name", "testw"
+        // const {name, value} = e.target
+        setNewCharacter({...newCharacter, [name]: value})
+    }
+
+    const handleSubmit = (e) => {
+        //step 0: prevent the default behavior
+        e.preventDefault() 
+        
+        // step 1: pack the data into a new object matching the structure from the db.json
+        // VERISION 1: 
+        // const newCharacter = {name, status, species, type, gender, image}
+        // VERSION 2 does not need this step because the info is already inside an object
+
+        //step 2 for VERSIONS 1 & 2: let the json server know about the new addition with a POST fetch request
+        fetch("http://localhost:3000/characters", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newCharacter)
+        })
+        //step 3 let the local state know about the new addition using setCharacters
+        .then(res => res.json())
+        .then(newData => setCharacters(currentListOfCharacters => [...currentListOfCharacters, newData]))
+
+        //step 4: empty the form MEANS resetting state to its initial value
+        //SOLUTION 1
+        // setName("")
+        // setImage("")
+        // setStatus("")
+        // setSpecies("")
+        // setType("")
+        // setGender("")
+
+        // SOLUTION 2
+        setNewCharacter({
+            name: "",
+            status: "",
+            species: "",
+            type: "",
+            gender: "",
+            image: ""
+        })
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="name">Name</label>
+                <input type="text" name="name" value={newCharacter.name} onChange={handleChange} />
+                <label htmlFor="image">Image Url</label>
+                <input type="text" name="image" value={newCharacter.image} onChange={handleChange} />
+                <label htmlFor="status">Status</label>
+                <input type="text" name="status" value={newCharacter.status} onChange={handleChange} />
+                <label htmlFor="species">Species</label>
+                <input type="text" name="species" value={newCharacter.species} onChange={handleChange} />
+                <label htmlFor="type">Type</label>
+                <input type="text" name="type" value={newCharacter.type} onChange={handleChange} />
+                <label htmlFor="gender">Gender</label>
+                <input type="text" name="gender" value={newCharacter.gender} onChange={handleChange} />
+                <input type="submit" value="Create" />
+            </form>
+        </div>
+    )
+}
+
+export default CharacterForm
